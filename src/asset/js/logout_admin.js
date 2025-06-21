73ebca03d93076b57e9946af5d0d9f6deedf50b4
Alpian Tabrani <atabrani3@gmail.com>
2025-06-21 20:19:12 +0800

    style: format code for consistency and readability in logout_admin.js

--- src/asset/js/logout_admin.js
+++ src/asset/js/logout_admin.js
@@ -1,37 +1,42 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-app.js";
-  import { getAuth, onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-auth.js";
+import {
+  getAuth,
+  onAuthStateChanged,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/11.9.0/firebase-auth.js";
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyDdd1vFusi_RKEazwc2Pc7wP39HUD6WM5E",
-    authDomain: "daftar-rusunawa.firebaseapp.com",
-    databaseURL: "https://daftar-rusunawa-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "daftar-rusunawa",
-    storageBucket: "daftar-rusunawa.firebasestorage.app",
-    messagingSenderId: "529594513792",
-    appId: "1:529594513792:web:8f4db9655d2cb6ca4e2965"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyDdd1vFusi_RKEazwc2Pc7wP39HUD6WM5E",
+  authDomain: "daftar-rusunawa.firebaseapp.com",
+  databaseURL:
+    "https://daftar-rusunawa-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "daftar-rusunawa",
+  storageBucket: "daftar-rusunawa.firebasestorage.app",
+  messagingSenderId: "529594513792",
+  appId: "1:529594513792:web:8f4db9655d2cb6ca4e2965",
+};
 
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-  // Cek login
-  onAuthStateChanged(auth, (user) => {
-    if (!user) {
-      window.location.href = "login.html";
-    }
-  });
+// Cek login
+onAuthStateChanged(auth, (user) => {
+  if (!user) {
+    window.location.href = "login.html";
+  }
+});
 
-  // Logout handler
-document.getElementById('logOut').addEventListener('click', () => {
+// Logout handler
+document.getElementById("logOut").addEventListener("click", () => {
   Swal.fire({
-    title: 'Yakin ingin keluar?',
+    title: "Yakin ingin keluar?",
     text: "Anda akan diarahkan ke halaman login",
-    icon: 'warning',
+    icon: "warning",
     showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Ya, keluar!',
-    cancelButtonText: 'Batal'
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Ya, keluar!",
+    cancelButtonText: "Batal",
   }).then((result) => {
     if (result.isConfirmed) {
       signOut(auth).then(() => {
@@ -39,4 +44,4 @@ document.getElementById('logOut').addEventListener('click', () => {
       });
     }
   });
-  });
+});
