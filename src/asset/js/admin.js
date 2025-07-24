18b1f0e50d37c8d4e1d713e286ce45b44415f46e
Alpian Tabrani <atabrani3@gmail.com>
2025-07-24 11:53:05 +0800

    total kamar merubah

--- src/asset/js/admin.js
+++ src/asset/js/admin.js
@@ -1,16 +1,21 @@
 // Import Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-app.js";
-import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/11.9.0/firebase-database.js";
+import {
+  getDatabase,
+  ref,
+  onValue,
+} from "https://www.gstatic.com/firebasejs/11.9.0/firebase-database.js";
 
 // Konfigurasi Firebase (ganti dengan punyamu)
 const firebaseConfig = {
-    apiKey: "AIzaSyDdd1vFusi_RKEazwc2Pc7wP39HUD6WM5E",
-    authDomain: "daftar-rusunawa.firebaseapp.com",
-    databaseURL: "https://daftar-rusunawa-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "daftar-rusunawa",
-    storageBucket: "daftar-rusunawa.firebasestorage.app",
-    messagingSenderId: "529594513792",
-    appId: "1:529594513792:web:8f4db9655d2cb6ca4e2965"
+  apiKey: "AIzaSyDdd1vFusi_RKEazwc2Pc7wP39HUD6WM5E",
+  authDomain: "daftar-rusunawa.firebaseapp.com",
+  databaseURL:
+    "https://daftar-rusunawa-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "daftar-rusunawa",
+  storageBucket: "daftar-rusunawa.firebasestorage.app",
+  messagingSenderId: "529594513792",
+  appId: "1:529594513792:web:8f4db9655d2cb6ca4e2965",
 };
 
 // Inisialisasi Firebase
@@ -18,14 +23,14 @@ const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
 // Elemen HTML target
-const kamarTerisiElem = document.getElementById('kamarTerisi');
-const kamarKosongElem = document.getElementById('kamarKosong');
-const totalKamar = document.getElementById('jumlahKamar');
+const kamarTerisiElem = document.getElementById("kamarTerisi");
+const kamarKosongElem = document.getElementById("kamarKosong");
+const totalKamar = document.getElementById("jumlahKamar");
 
-const TOTAL_KAMAR = 45;
+const TOTAL_KAMAR = 42; // Total kamar yang tersedia
 
 // Ambil jumlah penghuni dan hitung kamar
-onValue(ref(db, 'penghuni'), (snapshot) => {
+onValue(ref(db, "penghuni"), (snapshot) => {
   const jumlahPenghuni = snapshot.size || snapshot.numChildren();
   const kamarKosong = TOTAL_KAMAR - jumlahPenghuni;
 
